099408d70498ed6e38c5a7fd9e14002318fce92f
jason.chen <jason.chen@dkdtrillion.com>
2025-05-23 15:24:43 +0800

    完成功能

--- src/main/java/com/simpletalk/servlet/JavaChat.java
+++ src/main/java/com/simpletalk/servlet/JavaChat.java
@@ -16,67 +16,67 @@ import org.directwebremoting.ui.dwr.Util;
 import org.springframework.stereotype.Service;
 
 /**
- * 
  * chat server
- * 
  */
 @Slf4j
-@Service
+@Service("JavaChat")
 @RemoteProxy(name = "JavaChat")
 public class JavaChat {
-	private final LinkedList<Message> messages = new LinkedList<Message>();
-	private final Set<HttpSession> sessionSet = new HashSet<HttpSession>();
+    private final LinkedList<Message> messages = new LinkedList<Message>();
+    private final Set<HttpSession> sessionSet = new HashSet<HttpSession>();
 
-	@RemoteMethod
-	public void addMessage(String text) {
-		// Make sure we have a list of the list 10 messages
-		if (text != null && text.trim().length() > 0) {
-			if ("clearAll".equals(text)) {
-				messages.clear();
-				Util.setValue("text", "");
-				Util.setValue("chatlog", "");
-				return;
-			}
-			Message message = new Message(text);
-			message.setUsername(getSession().getAttribute("username").toString());
-			messages.addLast(message);
-		}
-		// Clear the input box in the browser that kicked off this page only
-		Util.setValue("text", "");
+    @RemoteMethod
+    public void addMessage(String text) {
+        // Make sure we have a list of the list 10 messages
+        if (text != null && text.trim().length() > 0) {
+            if ("clearAll".equals(text)) {
+                messages.clear();
+                Util.setValue("text", "");
+                Util.setValue("chatlog", "");
+                return;
+            }
+            Message message = new Message(text);
+            message.setUsername(getSession().getAttribute("username").toString());
+            messages.addLast(message);
+        }
+        // Clear the input box in the browser that kicked off this page only
+        Util.setValue("text", "");
 
-		// For all the browsers on the current page:
-		Browser.withCurrentPage(new Runnable() {
-			public void run() {
-				StringBuilder text = new StringBuilder();
-				for (Message m : messages) {
-					String content = m.getDate() + " [" + m.getUsername() + "]：" + m.getText();
-					text.append(content).append("\n");
-				}
-				Util.setValue("chatlog", "");
-				Util.setValue("chatlog", text.toString());
-			}
-		});
-	}
+        // For all the browsers on the current page:
+        Browser.withCurrentPage(new Runnable() {
+            public void run() {
+                StringBuilder text = new StringBuilder();
+                for (Message m : messages) {
+                    String content = m.getDate() + " [" + m.getUsername() + "]：" + m.getText();
+                    text.append(content).append("\n");
+                }
+                Util.setValue("chatlog", "");
+                Util.setValue("chatlog", text.toString());
+            }
+        });
+    }
 
-	private HttpSession getSession() {
-		WebContext ctx = WebContextFactory.get();
-		return ctx.getSession();
-	};
+    private HttpSession getSession() {
+        WebContext ctx = WebContextFactory.get();
+        return ctx.getSession();
+    }
 
-	@RemoteMethod
-	public void loginChat() {
-		log.info("[SimpleTalk] add session = {}", getSession());
-		sessionSet.add(getSession());
-		log.info("[SimpleTalk] sessionSet size = {}", sessionSet.size());
-		Util.setValue("allUsers", "在線人數:" + sessionSet.size());
-	}
+    ;
 
-	@RemoteMethod
-	public void logoutChat() {
-		log.info("[SimpleTalk] delete session = {} ", getSession());
-		sessionSet.remove(getSession());
-		getSession().removeAttribute("username");
-		log.info("[SimpleTalk] sessionSet size = {} ", sessionSet.size());
-		Util.setValue("allUsers", "在線人數:" + sessionSet.size());
-	}
+    @RemoteMethod
+    public void loginChat() {
+        log.info("[SimpleTalk] add session = {}", getSession());
+        sessionSet.add(getSession());
+        log.info("[SimpleTalk] sessionSet size = {}", sessionSet.size());
+        Util.setValue("allUsers", "在線人數:" + sessionSet.size());
+    }
+
+    @RemoteMethod
+    public void logoutChat() {
+        log.info("[SimpleTalk] delete session = {} ", getSession());
+        sessionSet.remove(getSession());
+        getSession().removeAttribute("username");
+        log.info("[SimpleTalk] sessionSet size = {} ", sessionSet.size());
+        Util.setValue("allUsers", "在線人數:" + sessionSet.size());
+    }
 }
